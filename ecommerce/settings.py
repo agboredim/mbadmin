1613a581582380cc48dad425166f46ff7dbc44a4
Agbor Edim <agborojating@gmail.com>
2025-04-19 05:15:54 -0400

    Your commit message

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -99,20 +99,23 @@ WSGI_APPLICATION = 'ecommerce.wsgi.application'
 
 
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'mbshawarmabite',
+#         'USER': 'postgres',
+#         'PASSWORD': '292611',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'mbshawarmabite',
-        'USER': 'postgres',
-        'PASSWORD': '292611',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
-
-
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
